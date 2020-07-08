37c2c0230688f1ac3a1d820ee355f0406fc80abb
John Sanda <john.sanda@gmail.com>
2020-07-07 22:47:02 -0400

    add logic for restore function

--- k8s/docker-entrypoint.sh
+++ k8s/docker-entrypoint.sh
@@ -6,12 +6,34 @@ echo "MEDUSA_MODE = $MEDUSA_MODE"
 
 restore() {
     echo "Running Medusa in restore mode"
+    if [ -z "$BACKUP_NAME" ]; then
+        echo "BACKUP_NAME env var not set, skipping restore operation"
+        exit 0
+    fi
+
+    if [ -a .last-restore]; then
+      backup_name=`cat .last-restore`
+      echo "Last restored backup is $backup_name"
+    else
+      backup_name=""
+    fi
+
+    if [ "$backup_name" == "$BACKUP_NAME"]; then
+        echo "Skipping restore operation"    
+    else
+        echo "Restoring backup $backup_name"
+        python3 -m medusa.service.grpc_svc.restore 
+        echo $backup_name > .last-restore
+    fi
 }
 
 grpc() {
     echo "Starting Medusa gRPC service"
     python3 -m medusa.service.grpc_svc.server server.py
 }
+
+echo "sleeping for $DEBUG_SLEEP sec"
+sleep $DEBUG_SLEEP
 
 if [ "$MEDUSA_MODE" == "RESTORE" ]; then
     restore
