8c5acbfe45af2e7287e143414c06f364d57da22d
John Sanda <john.sanda@gmail.com>
2020-07-13 10:52:35 -0400

    remove cyclic import to see if it fixes CI failure
    In this commit, I duplicated the evaluate_boolean function. If this change
    fixes the error, then I will replace medusa.config.evaluate_boolean with this
    change.

--- medusa/cassandra_utils.py
+++ medusa/cassandra_utils.py
@@ -29,7 +29,7 @@ import time
 import uuid
 import yaml
 import requests
-import medusa.config
+import medusa.utils
 
 from subprocess import PIPE
 from retrying import retry
@@ -420,7 +420,7 @@ class Cassandra(object):
         # Eventually I think we will want to introduce an abstraction layer for Cassandra's
         # API that Medusa requires. There should be an implementation for using nodetool,
         # one for Jolokia, and a 3rd for the management sidecard used by Cass Operator.
-        if medusa.config.evaluate_boolean(self.grpc_config.enabled):
+        if medusa.utils.evaluate_boolean(self.grpc_config.enabled):
             data = {
                 "type": "exec",
                 "mbean": "org.apache.cassandra.db:type=StorageService",
@@ -442,7 +442,7 @@ class Cassandra(object):
     def delete_snapshot(self, tag):
         cmd = self._nodetool.nodetool + ['clearsnapshot', '-t', tag]
 
-        if medusa.config.evaluate_boolean(self.grpc_config.enabled):
+        if medusa.utils.evaluate_boolean(self.grpc_config.enabled):
             data = {
                 "type": "exec",
                 "mbean": "org.apache.cassandra.db:type=StorageService",
