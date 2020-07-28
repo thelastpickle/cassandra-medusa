4b4a963c6bad8b60779ad641bb0e88b03de9720f
Alain Rodriguez <arodrime@gmail.com>
2020-07-28 12:57:09 +0200

    Do not rely on nc command output, use returncode instead

--- medusa/cassandra_utils.py
+++ medusa/cassandra_utils.py
@@ -631,8 +631,9 @@ def is_ccm_up(args, nodetool_command):
 def is_cassandra_up(args, port):
     try:
         args.append(port)
-        output = subprocess.check_output(args, stderr=subprocess.STDOUT, universal_newlines=True)
-        return output.find('succeeded') >= 0
+        subprocess.check_call(args)
+        # check_call returns if call succeeded (returncode is 0). If not exception is triggered
+        return True
     except subprocess.CalledProcessError:
         logging.debug('The node is not up yet')
         return False
