c77f50eeeccd442e6291e9b70101a1d4bd3751e3
Brice Figureau <brice@daysofwonder.com>
2020-10-12 12:39:57 +0200

    (#201) Improved debian packaging
    This change adds a series of fix and improvements to the debian
    packaging.
    
    Because a package built on bionic might not work on an older
    distribution, we now build all debien packages on their respective
    distributions.
    
    It is possible to build debian packages for: Ubuntu bionic, Ubuntu
    xenial, Debian buster and Debian stretch
    
    The release workflow has been modified to now uploads packages
    for all distributions in their respective distributions to Bintray.
    Note: since it is not possible to reupload a PyPi package, the debian
    upload step has been moved to run first. So if it fails, it will be
    possible to retry it without having to change the version number.
    
    Finally, build-deb.sh has been modified to by default build only
    bionic. It is possible to build all distribution by pasing 'all' as
    argument, or a specific ditribution (ie bionic, xenial, buster or
    stretch).

--- packaging/build-deb.sh
+++ packaging/build-deb.sh
@@ -1,7 +1,24 @@
 #!/usr/bin/env bash
 
-set -xe
+set -e
+
+case $1 in
+  ""|bionic|xenial|buster|stretch)
+    suites=("${1:-bionic}")
+    ;;
+  all)
+    suites=(bionic xenial buster stretch)
+    ;;
+  *)
+    echo "Unknown distribution suite - allowed values: 'all', 'bionic', 'xenial', 'buster', 'stretch'"
+    exit 1
+    ;;
+esac
 
 cd docker-build
-docker-compose build
-docker-compose run cassandra-medusa-builder
+
+for suite in "${suites[@]}"
+do
+  docker-compose build "cassandra-medusa-builder-${suite}"
+  docker-compose run "cassandra-medusa-builder-${suite}"
+done
