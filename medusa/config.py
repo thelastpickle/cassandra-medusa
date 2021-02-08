e1664c141f4e6ab2b80ac7c50946260bc7c85b60
Ivan Mora Perez <ivan@opentodo.net>
2021-02-08 19:39:01 +0100

    Add support for s3 canned ACLs for uploaded objects

--- medusa/config.py
+++ medusa/config.py
@@ -31,7 +31,8 @@ StorageConfig = collections.namedtuple(
     ['bucket_name', 'key_file', 'prefix', 'fqdn', 'host_file_separator', 'storage_provider', 'storage_class',
      'base_path', 'max_backup_age', 'max_backup_count', 'api_profile', 'transfer_max_bandwidth',
      'concurrent_transfers', 'multi_part_upload_threshold', 'host', 'region', 'port', 'secure', 'ssl_verify',
-     'aws_cli_path', 'kms_id', 'backup_grace_period_in_days', 'use_sudo_for_restore', 'k8s_mode', 'read_timeout']
+     'aws_cli_path', 'kms_id', 'backup_grace_period_in_days', 'use_sudo_for_restore', 'k8s_mode', 'read_timeout',
+     'canned_acl']
 )
 
 CassandraConfig = collections.namedtuple(
@@ -117,7 +118,8 @@ def _build_default_config():
         'region': 'default',
         'backup_grace_period_in_days': 10,
         'use_sudo_for_restore': 'True',
-        'read_timeout': 60
+        'read_timeout': 60,
+        'canned_acl': 'private',
     }
 
     config['logging'] = {
