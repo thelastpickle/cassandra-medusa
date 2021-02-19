6a0ad6776b79e871cca7a9edf5de2baf65a15283
Alexander Dejanovski <alex@thelastpickle.com>
2021-02-19 14:44:13 +0100

    Pin cryptography to 3.3.2 to avoid install failing on missing Rust req
    Paramiko, which is pulled as a dep by pssh uses the cryptography dependency with just a lower bound (needs to be >= 2.5).
    v3.4.6 of cryptography apparently have a requirement on setuptools_rust which fails the installs in several cases.

--- setup.py
+++ setup.py
@@ -51,7 +51,8 @@ setuptools.setup(
         'ffwd>=0.0.2',
         'apache-libcloud<=3.3.0,>=2.8.0',
         'lockfile>=0.12.2',
-        'pycrypto>=2.6.1',
+        'cryptography<=3.3.2,>=2.5',
+        'pycryptodome>=3.9.9',
         'retrying>=1.3.3',
         'parallel-ssh==1.9.1',
         'ssh2-python==0.19.0',  # <-- ssh2-python==0.20.0 is broken, 0.22.0+ should work.
