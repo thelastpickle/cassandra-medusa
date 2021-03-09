af0cc38ce563c4a6214e1e3236d635187feeb67f
Alexander Dejanovski <alex@thelastpickle.com>
2021-03-09 15:17:44 +0100

    Bump version: 0.9.1 → 0.9.2-dev

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='cassandra-medusa',
-    version='0.9.1',
+    version='0.9.2-dev',
     author='The Last Pickle',
     author_email='medusa@thelastpickle.com',
     url='https://github.com/thelastpickle/cassandra-medusa',
