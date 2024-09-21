a7d4f2d92ae24a662a36061f13bb78dc92c3cd1d
Tibor Répási <rtib@users.noreply.github.com>
2024-09-21 10:14:04 +0200

    add build for debian-bookworm

--- packaging/build-deb.sh
+++ packaging/build-deb.sh
@@ -3,14 +3,14 @@
 set -e
 
 case $1 in
-  ""|bionic|buster|focal|bullseye)
+  ""|bionic|buster|focal|bullseye|bookworm)
     suites=("${1:-bionic}")
     ;;
   all)
-    suites=(focal bionic buster bullseye)
+    suites=(focal bionic buster bullseye bookworm)
     ;;
   *)
-    echo "Unknown distribution suite - allowed values: 'all', 'bionic', 'buster', 'focal', 'bullseye'"
+    echo "Unknown distribution suite - allowed values: 'all', 'bionic', 'buster', 'focal', 'bullseye', 'bookworm'"
     exit 1
     ;;
 esac
