3cd9a1fbc454c7cb2f8499a155639a4f41304c7a
Alain Rodriguez <alain@casterix.fr>
2026-07-09 15:11:49 +0200

    Stop filtering 0-byte objects in list_objects, use Azure dir-marker metadata instead

--- tests/storage/azure_storage_test.py
+++ tests/storage/azure_storage_test.py
@@ -5,6 +5,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import asyncio
 import os
 import tempfile
 import unittest
@@ -107,3 +108,50 @@ class AzureStorageTest(unittest.TestCase):
             'https://testAccount.blob.core.windows.net/',
             azure_storage.azure_blob_service_url
         )
+
+    def test_list_blobs_skips_directory_placeholders(self):
+        with tempfile.NamedTemporaryFile() as credentials_file:
+            credentials_file.write(self.credentials_file_content.encode())
+            credentials_file.flush()
+            config = AttributeDict({
+                'region': 'region-from-config',
+                'storage_provider': 'azure_blobs',
+                'key_file': credentials_file.name,
+                'bucket_name': 'bucket-from-config',
+                'concurrent_transfers': '1',
+                'host': None,
+                'port': None,
+                'read_timeout': 60,
+            })
+            azure_storage = AzureStorage(config)
+            azure_storage.connect()
+
+            async def fake_list_blobs(name_starts_with=None, include=None, timeout=None):
+                for props in (
+                    # ADLS Gen2 / hierarchical-namespace directory marker: must be skipped
+                    AttributeDict({
+                        'name': 'some/dir/',
+                        'size': 0,
+                        'etag': 'etag1',
+                        'last_modified': None,
+                        'blob_tier': None,
+                        'metadata': {'hdi_isfolder': 'true'},
+                    }),
+                    # legitimately empty SSTable component (e.g. BTI Rows.db): must be kept
+                    AttributeDict({
+                        'name': 'some/dir/da-1-bti-Rows.db',
+                        'size': 0,
+                        'etag': 'etag2',
+                        'last_modified': None,
+                        'blob_tier': None,
+                        'metadata': {},
+                    }),
+                ):
+                    yield props
+
+            azure_storage.azure_container_client.list_blobs = fake_list_blobs
+
+            blobs = asyncio.run(azure_storage._list_blobs())
+
+            self.assertEqual(1, len(blobs))
+            self.assertEqual('some/dir/da-1-bti-Rows.db', blobs[0].name)
